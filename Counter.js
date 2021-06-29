7632a1e15ac7529c86ddaae26e5d8c6a060e1716
Fatih Şen <fatih.sn2000@gmail.com>
2021-06-29 11:01:03 +0300

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Counter(){
+    const[count,setCount] = useState(0);
+    const increase = () => setCount(count + 1);
+    const decrease = () => setCount(count - 1);
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick = {decrease}>decrease</button>
+            <button onClick = {increase}>increase</button>
+        </div>
+    )
+    
+}
+export default Counter;
